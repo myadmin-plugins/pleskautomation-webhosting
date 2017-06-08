ae6b87536381680f3f8531930a2dfd5c26a87ad5
Joe Huss <detain@interserver.net>
2017-06-08 00:59:35 -0400

    plugin definition updates

--- myadmin.php
+++ myadmin.php
@@ -8,17 +8,17 @@ return [
 	'name' => 'Pleskautomation Webhosting',
 	'description' => 'Allows selling of Pleskautomation Server and VPS License Types.  More info at https://www.netenberg.com/pleskautomation.php',
 	'help' => 'It provides more than one million end users the ability to quickly install dozens of the leading open source content management systems into their web space.  	Must have a pre-existing cPanel license with cPanelDirect to purchase a pleskautomation license. Allow 10 minutes for activation.',
-	'module' => 'licenses',
+	'module' => 'webhosting',
 	'author' => 'detain@interserver.net',
 	'home' => 'https://github.com/detain/myadmin-pleskautomation-webhosting',
 	'repo' => 'https://github.com/detain/myadmin-pleskautomation-webhosting',
 	'version' => '1.0.0',
-	'type' => 'licenses',
+	'type' => 'service',
 	'hooks' => [
 		/*'function.requirements' => ['Detain\MyAdminPleskautomation\Plugin', 'Requirements'],
-		'licenses.settings' => ['Detain\MyAdminPleskautomation\Plugin', 'Settings'],
-		'licenses.activate' => ['Detain\MyAdminPleskautomation\Plugin', 'Activate'],
-		'licenses.change_ip' => ['Detain\MyAdminPleskautomation\Plugin', 'ChangeIp'],
+		'webhosting.settings' => ['Detain\MyAdminPleskautomation\Plugin', 'Settings'],
+		'webhosting.activate' => ['Detain\MyAdminPleskautomation\Plugin', 'Activate'],
+		'webhosting.change_ip' => ['Detain\MyAdminPleskautomation\Plugin', 'ChangeIp'],
 		'ui.menu' => ['Detain\MyAdminPleskautomation\Plugin', 'Menu'] */
 	],
 ];
