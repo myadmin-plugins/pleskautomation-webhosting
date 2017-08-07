4435939e29af041ac0c6ef98db934afb85d9e65f
Joe Huss <detain@interserver.net>
2017-08-07 16:50:40 -0400

    o a buch a bunch of fixs and improvements

--- src/get_webhosting_ppa_instance.php
+++ src/get_webhosting_ppa_instance.php
@@ -2,7 +2,7 @@
 /**
  * Gets a Plesk Automation Class instance for the given server.
  *
- * @param array|bool|false|int $server the server to get a Plesk Automation instance for, can be an array like from get_service or a server id, or false for default
+ * @param array|bool|false|int|string $server the server to get a Plesk Automation instance for, can be an array like from get_service or a server id, or false for default
  * @return \Detain\MyAdminPleskAutomation\PPAConnector the pleskautomation instance
  */
 function get_webhosting_ppa_instance($server = FALSE) {
