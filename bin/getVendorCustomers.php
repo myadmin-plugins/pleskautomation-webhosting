b7c071f111a10f048e163bdf9c2e92eaa8c79ab2
Joe Huss <detain@interserver.net>
2017-12-24 08:16:31 -0500

    new file for working w/ the new codee

---
+++ bin/getVendorCustomers.php
@@ -0,0 +1,15 @@
+<?php
+include_once __DIR__.'/../../../../include/functions.inc.php';
+$ppaConnector = get_webhosting_ppa_instance();
+$accountId = (int) $_SERVER['argv'][1];
+$request = [
+	'account_id' => $accountId
+];
+$result = $ppaConnector->getAccountMembers($request);
+echo preg_replace("/$\s*array\s+\(/msiU", 'array(', var_export($result, TRUE));
+echo "\n";
+try {
+	PPAConnector::checkResponse($result);
+} catch (Exception $e) {
+	echo 'Caught exception: '.$e->getMessage().PHP_EOL;
+}
